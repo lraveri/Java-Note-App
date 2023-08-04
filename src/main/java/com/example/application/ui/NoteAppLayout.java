117e605ebec9bd25cecf1ec973ba59aa12c23782
Luca Raveri <lucaraveri993@gmail.com>
2023-08-04 17:18:12 +0200

    ui

--- src/main/java/com/example/application/ui/NoteAppLayout.java
+++ src/main/java/com/example/application/ui/NoteAppLayout.java
@@ -5,11 +5,11 @@ import com.example.application.backend.service.NoteService;
 import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,31 +17,44 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Route("")
 public class NoteAppLayout extends VerticalLayout {
 
-    @Autowired
     private NoteService noteService;
 
-    private H1 title;
+    private H1 title = new H1("My Note App");;
 
-    private FormLayout formLayout;
+    private FormLayout formLayout = new FormLayout();
 
-    public NoteAppLayout() {
+    Grid<Note> grid = new Grid<>(Note.class);
 
-        title = new H1("My Note App");
-        configureTitle();
+    @Autowired
+    public NoteAppLayout(NoteService noteService) {
+        this.noteService = noteService;
 
-        formLayout = new FormLayout();
         configureFormLayout();
 
-        add(title, formLayout);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        configureGrid();
 
+        add(title, formLayout, grid);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+    }
+
+    private void updateList() {
+        grid.setItems(noteService.findAll());
     }
 
     private void configureFormLayout() {
         TextField title = new TextField("Title");
         TextField content = new TextField();
         content.setPlaceholder("Insert text...");
-        //content.setHeight(3, Unit.CM);
+        Button button = getButton(title, content);
+        formLayout.add(title, content, button);
+        formLayout.setColspan(title, 1);
+        formLayout.setColspan(content, 2);
+        formLayout.setColspan(button, 2);
+        formLayout.setSizeFull();
+        formLayout.setMaxWidth(50, Unit.PERCENTAGE);
+    }
+
+    private Button getButton(TextField title, TextField content) {
         Button button = new Button("Add note");
         button.addClickListener(event -> {
             String titleText = title.getValue();
@@ -54,19 +67,19 @@ public class NoteAppLayout extends VerticalLayout {
                 title.clear();
                 content.clear();
                 Notification.show("Note created!");
+                updateList();
             } else {
                 Notification.show("Insert title and text!");
             }
         });
-        formLayout.add(title, content, button);
-        formLayout.setColspan(title, 1);
-        formLayout.setColspan(content, 2);
-        formLayout.setColspan(button, 2);
-        formLayout.setSizeFull();
+        button.setMaxWidth(3, Unit.CM);
+        return button;
     }
 
-    private void configureTitle() {
+    private void configureGrid() {
+        grid.setColumns("id", "title", "content");
     }
+
 }
 
 
