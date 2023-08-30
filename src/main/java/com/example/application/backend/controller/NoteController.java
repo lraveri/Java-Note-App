d17ca52d27cec25eee831cf15eceba662d9cef3d
Christian Luzzetti <christian.luzzetti@gmail.com>
2023-08-30 22:41:09 +0200

    simplified http mappings
    @RestController consumes and produces MediaType.APPLICATION_JSON_VALUE by default
    
    Moved the common part of the endpoints "/note/" to a class level @RequestMapping

--- src/main/java/com/example/application/backend/controller/NoteController.java
+++ src/main/java/com/example/application/backend/controller/NoteController.java
@@ -5,34 +5,34 @@ import com.example.application.backend.service.NoteService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("note")
 @RequiredArgsConstructor
 @Log4j2
 public class NoteController {
 
   private final NoteService noteService;
 
-  @GetMapping(value = "/note/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<Object> findNote(@PathVariable Long id) {
+  @GetMapping("{id}")
+  public ResponseEntity<Note> findNote(@PathVariable Long id) {
     return new ResponseEntity<>(noteService.findById(id), HttpStatus.OK);
   }
 
-  @PostMapping(value = "/note", produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<Object> createNote(@RequestBody Note note) {
+  @PostMapping
+  public ResponseEntity<Note> createNote(@RequestBody Note note) {
     return new ResponseEntity<>(noteService.createNote(note), HttpStatus.OK);
   }
 
-  @PutMapping(value = "/note/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<Object> updateNote(@RequestBody Note note) {
+  @PutMapping("{id}")
+  public ResponseEntity<Note> updateNote(@RequestBody Note note) {
     return new ResponseEntity<>(noteService.updateNote(note), HttpStatus.OK);
   }
 
-  @DeleteMapping(value = "/note/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<Object> deleteNote(@PathVariable Long id) {
+  @DeleteMapping("{id}")
+  public ResponseEntity<Note> deleteNote(@PathVariable Long id) {
     noteService.deleteNote(id);
     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
   }
--- src/test/java/com/example/application/backend/repository/NoteRepositoryTestDiffBlue.java
+++ src/test/java/com/example/application/backend/repository/NoteRepositoryTestDiffBlue.java
@@ -1,14 +1,9 @@
 package com.example.application.backend.repository;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import com.example.application.backend.model.entity.Note;
-
-import java.sql.Date;
-
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -22,52 +17,40 @@ import org.springframework.test.context.ContextConfiguration;
 @DataJpaTest
 class NoteRepositoryTestDiffBlue {
 
-    @Autowired
-    private NoteRepository noteRepository;
+  @Autowired private NoteRepository noteRepository;
 
-    /**
-     * Method under test: {@link NoteRepository#findAllByCreatedBy(String)}
-     */
-    @Test
-    void testFindAllByCreatedBy() {
-        Date createdWhen = mock(Date.class);
-        when(createdWhen.getTime()).thenReturn(10L);
-        Date lastUpdatedWhen = mock(Date.class);
-        when(lastUpdatedWhen.getTime()).thenReturn(10L);
+  /** Method under test: {@link NoteRepository#findAllByCreatedBy(String)} */
+  @Test
+  void testFindAllByCreatedBy() {
+    LocalDateTime createdWhen = LocalDateTime.of(2020, 1, 1, 8, 0);
+    LocalDateTime lastUpdatedWhen = LocalDateTime.of(2020, 1, 1, 8, 0);
 
-        Note note = new Note();
-        note.setContent("Not all who wander are lost");
-        note.setCreatedBy("Jan 1, 2020 8:00am GMT+0100");
-        note.setCreatedWhen(createdWhen);
-        note.setIdParent(1L);
-        note.setIsPinned(1L);
-        note.setLastUpdatedWhen(lastUpdatedWhen);
-        note.setTags("Tags");
-        note.setTitle("Dr");
-        Date createdWhen2 = mock(Date.class);
-        when(createdWhen2.getTime()).thenReturn(10L);
-        Date lastUpdatedWhen2 = mock(Date.class);
-        when(lastUpdatedWhen2.getTime()).thenReturn(10L);
+    Note note = new Note();
+    note.setContent("Not all who wander are lost");
+    note.setCreatedBy("Jan 1, 2020 8:00am GMT+0100");
+    note.setCreatedWhen(createdWhen);
+    note.setIdParent(1L);
+    note.setIsPinned(1L);
+    note.setLastUpdatedWhen(lastUpdatedWhen);
+    note.setTags("Tags");
+    note.setTitle("Dr");
 
-        Note note2 = new Note();
-        note2.setContent("Content");
-        note2.setCreatedBy("Created By");
-        note2.setCreatedWhen(createdWhen2);
-        note2.setIdParent(2L);
-        note2.setIsPinned(-1L);
-        note2.setLastUpdatedWhen(lastUpdatedWhen2);
-        note2.setTags("com.example.application.backend.model.entity.Note");
-        note2.setTitle("Mr");
+    LocalDateTime createdWhen2 = LocalDateTime.of(2020, 1, 1, 8, 0);
+    LocalDateTime lastUpdatedWhen2 = LocalDateTime.of(2020, 1, 1, 8, 0);
 
-        noteRepository.save(note);
-        noteRepository.save(note2);
+    Note note2 = new Note();
+    note2.setContent("Content");
+    note2.setCreatedBy("Created By");
+    note2.setCreatedWhen(createdWhen2);
+    note2.setIdParent(2L);
+    note2.setIsPinned(-1L);
+    note2.setLastUpdatedWhen(lastUpdatedWhen2);
+    note2.setTags("com.example.application.backend.model.entity.Note");
+    note2.setTitle("Mr");
 
-        assertTrue(noteRepository.findAllByCreatedBy("janedoe").isEmpty());
+    noteRepository.save(note);
+    noteRepository.save(note2);
 
-        verify(createdWhen).getTime();
-        verify(lastUpdatedWhen).getTime();
-        verify(createdWhen2).getTime();
-        verify(lastUpdatedWhen2).getTime();
-    }
+    assertTrue(noteRepository.findAllByCreatedBy("janedoe").isEmpty());
+  }
 }
-
