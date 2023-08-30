722251094015d1f8fc3ef42f39b9f70f71f112a8
Luca Raveri <lucaraveri993@gmail.com>
2023-08-30 15:01:44 +0200

    integration test

---
+++ src/test/java/com/example/application/backend/integration/NoteControllerIntegrationTest.java
@@ -0,0 +1,59 @@
+package com.example.application.backend.integration;
+
+import com.example.application.backend.model.entity.Note;
+import com.example.application.backend.repository.NoteRepository;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class NoteControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private NoteRepository noteRepository;
+
+    private Note note;
+
+    @BeforeEach
+    void setup() {
+        note = Note.builder()
+                .id(1L)
+                .title("note")
+                .build();
+
+        noteRepository.save(note);
+    }
+
+    @Test
+    @WithMockUser(username = "user", roles = "USER")
+    void givenNoteObject_whenFindNoteById_thenReturnNote() throws Exception {
+
+        // given
+
+        // when
+        ResultActions response = mockMvc.perform(get("/note/{id}", 1L));
+
+        // then
+        response.andDo(MockMvcResultHandlers.print());
+
+        response.andExpect(status().isOk())
+                .andExpect(jsonPath("$.title", CoreMatchers.is(note.getTitle())));
+    }
+
+}
