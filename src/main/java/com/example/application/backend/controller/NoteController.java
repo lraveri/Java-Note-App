e468180fe123bccc1bde7bf04b2359fe5c9e07f4
Christian Luzzetti <christian.luzzetti@gmail.com>
2023-08-30 22:44:59 +0200

    simplified and typed ResponseEntity construction
    ResponseEntity object has a Static Factory Methods to be instantiated
    
    ResponseEntity.ok();

--- src/main/java/com/example/application/backend/controller/NoteController.java
+++ src/main/java/com/example/application/backend/controller/NoteController.java
@@ -4,7 +4,6 @@ import com.example.application.backend.model.entity.Note;
 import com.example.application.backend.service.NoteService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,22 +17,22 @@ public class NoteController {
 
   @GetMapping("{id}")
   public ResponseEntity<Note> findNote(@PathVariable Long id) {
-    return new ResponseEntity<>(noteService.findById(id), HttpStatus.OK);
+    return ResponseEntity.ok(noteService.findById(id));
   }
 
   @PostMapping
   public ResponseEntity<Note> createNote(@RequestBody Note note) {
-    return new ResponseEntity<>(noteService.createNote(note), HttpStatus.OK);
+    return ResponseEntity.ok(noteService.createNote(note));
   }
 
   @PutMapping("{id}")
   public ResponseEntity<Note> updateNote(@RequestBody Note note) {
-    return new ResponseEntity<>(noteService.updateNote(note), HttpStatus.OK);
+    return ResponseEntity.ok(noteService.updateNote(note));
   }
 
   @DeleteMapping("{id}")
   public ResponseEntity<Note> deleteNote(@PathVariable Long id) {
     noteService.deleteNote(id);
-    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    return ResponseEntity.noContent().build();
   }
 }
