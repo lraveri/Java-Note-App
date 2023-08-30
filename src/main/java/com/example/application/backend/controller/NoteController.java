e0bd3ba4d9dce37f4b75fababc6998158c2f6a37
Christian Luzzetti <christian.luzzetti@gmail.com>
2023-08-30 22:28:14 +0200

    removed deprecated field injection, in favor of constructor injection
    (The use of @RequiredArgsConstructor automatically generates a constructor, ready to inject the final fields)

--- src/main/java/com/example/application/backend/controller/NoteController.java
+++ src/main/java/com/example/application/backend/controller/NoteController.java
@@ -2,37 +2,38 @@ package com.example.application.backend.controller;
 
 import com.example.application.backend.model.entity.Note;
 import com.example.application.backend.service.NoteService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
+@Log4j2
 public class NoteController {
 
-    @Autowired
-    NoteService noteService;
+  private final NoteService noteService;
 
-    @GetMapping(value = "/note/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Object> findNote(@PathVariable Long id) {
-        return new ResponseEntity<>(noteService.findById(id), HttpStatus.OK);
-    }
+  @GetMapping(value = "/note/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Object> findNote(@PathVariable Long id) {
+    return new ResponseEntity<>(noteService.findById(id), HttpStatus.OK);
+  }
 
-    @PostMapping(value = "/note", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Object> createNote(@RequestBody Note note) {
-        return new ResponseEntity<>(noteService.createNote(note), HttpStatus.OK);
-    }
+  @PostMapping(value = "/note", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Object> createNote(@RequestBody Note note) {
+    return new ResponseEntity<>(noteService.createNote(note), HttpStatus.OK);
+  }
 
-    @PutMapping(value = "/note/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Object> updateNote(@RequestBody Note note) {
-        return new ResponseEntity<>(noteService.updateNote(note), HttpStatus.OK);
-    }
+  @PutMapping(value = "/note/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Object> updateNote(@RequestBody Note note) {
+    return new ResponseEntity<>(noteService.updateNote(note), HttpStatus.OK);
+  }
 
-    @DeleteMapping(value = "/note/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Object> deleteNote(@PathVariable Long id) {
-        noteService.deleteNote(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-
+  @DeleteMapping(value = "/note/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Object> deleteNote(@PathVariable Long id) {
+    noteService.deleteNote(id);
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
 }
